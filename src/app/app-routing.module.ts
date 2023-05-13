4933b0408bd04dee598aad8c8f46f5ebf2c52bab
sonfackyann <sonfackyann@gmail.com>
2023-05-13 18:18:51 +0100

    list of events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { TicketListComponent } from './component/ticket-list/ticket-list.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
 
 const routes: Routes = [
   {
@@ -8,7 +10,9 @@ const routes: Routes = [
     redirectTo: '/home',
     pathMatch: 'full'
   },
-  {path:'home', component:HomeComponent}
+  {path:'home', component:HomeComponent},
+  {path:'home/:id',component:TicketListComponent},
+  {path:'checkout', component:CheckoutComponent}
 ];
 
 @NgModule({
